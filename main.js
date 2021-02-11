b29b793340ed156b418296b44dce3eded84d5bd6
Pranjal Shrivastav <pranjalsrivastav07@gmail.com>
2021-02-11 21:29:37 +0530

    layout correct

--- main.js
+++ main.js
@@ -1,44 +1,22 @@
-// let local = document.querySelectorAll('.project')
-// let local2 = local
+var ham = document.getElementById('ham')
+var cross = document.getElementById('cross')
+var ul = document.getElementById('ul')
 
-// function hoverEffect(local, local2) {
 
-//     for (let index = 0; index < local2.length; index++) {
+if(window,screen.width <= 500){
 
-//         local[index].addEventListener('mouseenter', () => {
-//             var num = local2[index].children
-//             var count = 0
-//             // console.log(num[index])
-//             // local2[index].style.backgroundColor = 'white'
-//             // local2[index].style.transition = 'all 1s ease-in-out'
-//             num[count].style.display = 'none'
-//             num[count + 1].style.transform = 'translateY(0px)'
-//             num[count + 1].style.display = 'block'
-//         })
-        
-//         local[index].addEventListener('mouseleave', () => {
-//             var num = local2[index].children
-//             var count = 0
-//             // console.log(num[index])
-//             // local2[index].style.backgroundColor = 'transparent'
-//             // local2[index].style.transition = 'all 1s ease-in-out'
-//             num[count].style.display = 'block'
-
-//             num[count+1].style.display = 'none'
-//             num[count + 1].style.transform = 'translateY(400px)'
-//         })
-
-//     }
-// }
-
-// hoverEffect(local, local2)
-
-// let skill = document.querySelectorAll('.skill')
-// let skill2 = skill
-
-// hoverEffect(skill, skill2)
-var mail = document.getElementById('mail')
-
-mail.addEventListener('click', ()=>{
-    window.location.href = 'mailto:pranjalsrivastav07@gmail.com'
+ham.addEventListener('click',()=>{
+    ul.style.transition = 'all ease-in-out 2s'
+    ham.style.display = 'none'
+    ul.style.display = 'block'
+    cross.style.display = 'block'
 })
+
+cross.addEventListener('click', ()=>{
+    ul.style.transition = 'all ease-in-out 2s'
+    ham.style.display = 'block'
+    ul.style.display = 'none'
+    cross.style.display = 'none'
+})
+}
+
